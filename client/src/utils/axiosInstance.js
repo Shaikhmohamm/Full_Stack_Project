200ce8d69467c7e6c53f80b979622e52b86868e6
zoha shaikh <zoha@pleximus.net>
2024-05-17 01:35:23 +0530

    Added live Backend URL

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Create an axios instance with the base URL set to localhost and enabling credentials
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001/api'
+    baseURL: 'https://full-stack-project-entertainment-app.onrender.com/api'
 });
 
 // Export the axios instance for making HTTP requests
 export default axiosInstance;
+
